f31dc7802d8f3f134952b90f1c3ab2e9bb0d8af7
ChanYoung <113960643+CanyonHug@users.noreply.github.com>
2023-05-21 16:44:48 +0900

    Update binance-websocket.py
    There was an error with kline data.

--- binance-websocket.py
+++ binance-websocket.py
@@ -5,13 +5,22 @@ import websockets
 from datetime import datetime
 
 async def save_order_book():
-    url = 'wss://stream.binance.com:9443/ws/btcusdt@depth5'  # Replace with your desired symbol and depth level
+    # partial book depth stream
+    # update speed: 500ms = 0.5s
+    url = 'wss://fstream.binance.com/ws/btcusdt@depth10@500ms'  # Replace with your desired symbol and depth level
+    kline_url = 'wss://fstream.binance.com/ws/btcusdt@kline_5m' # for kline data using at last step
 
-    data = []
+    
+    num = 469   # should be updated as the last number of the file + 1
 
-    async with websockets.connect(url) as websocket:
-        while True:
-            try:
+    while True:
+        data = []
+
+        try:
+            websocket = await websockets.connect(url)
+            kline_websocket = await websockets.connect(kline_url)
+
+            while True:
                 response = await websocket.recv()
                 order_book = json.loads(response)
 
@@ -19,19 +28,29 @@ async def save_order_book():
                 data.append(order_book)
 
                 # Convert to pandas DataFrame every 10 minutes
-                if len(data) % 300 == 0:  # 5 minutes = 5 * 60 seconds = 300 seconds
+                if len(data) % 600 == 0:  # 5 minutes = 5 * 60 seconds = 5 * 60 * 2 = 600 (0.5 seconds)
+                    kline_response = await kline_websocket.recv()
+                    kline = json.loads(kline_response)
+                    data.append(kline)
+
                     df = pd.DataFrame(data)
                     now = datetime.now()
-                    df.to_json(f'{now.year}-{now.month}-{now.day}-{now.hour}-{now.minute}.json')
-                    print("Order book data saved to JSON file.")
+                    df.to_json(f'./LOB/{num}.json')
+                    print(f"{now.year}-{now.month}-{now.day}-{now.hour}-{now.minute} Order book data saved to JSON file.")
                     data = []  # Reset data
+                    num += 1
 
-            except websockets.exceptions.ConnectionClosedOK:
-                print("WebSocket connection closed.")
-                break
+        except websockets.exceptions.ConnectionClosedOK:
+            print("WebSocket connection closed. Reconnecting...")
+            continue
 
-            except Exception as e:
-                print(f"An error occurred: {e}")
+        except websockets.exceptions.WebSocketException as e:
+            print(f"WebSocket exception occurred: {e}")
+            continue
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            continue
 
 async def main():
     await save_order_book()
